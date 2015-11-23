9502efcffa8916a5482b7e419c4787fb0db2c52f
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-11-23 23:16:14 +0100

    fix stream connection url connection address and make the connection not be always ssl because when using a proxy connecting to it using ssl is uncommond and problematic

--- lib/twitter/streaming/connection.rb
+++ lib/twitter/streaming/connection.rb
@@ -10,18 +10,27 @@ module Twitter
       def initialize(options = {})
         @tcp_socket_class = options.fetch(:tcp_socket_class) { TCPSocket }
         @ssl_socket_class = options.fetch(:ssl_socket_class) { OpenSSL::SSL::SSLSocket }
+        @use_ssl          = options.fetch(:use_ssl) { false }
       end
 
       def stream(request, response)
-        client_context = OpenSSL::SSL::SSLContext.new
-        client         = @tcp_socket_class.new(Resolv.getaddress(request.uri.host), request.uri.port)
-        ssl_client     = @ssl_socket_class.new(client, client_context)
+        socket = @tcp_socket_class.new(Resolv.getaddress(request.socket_host), request.socket_port)
+        if !request.using_proxy? || (request.using_proxy? && @use_ssl)
+          socket = ssl_stream(socket)
+        end
 
-        ssl_client.connect
-        request.stream(ssl_client)
-        while body = ssl_client.readpartial(1024) # rubocop:disable AssignmentInCondition
+        request.stream(socket)
+        while body = socket.readpartial(1024) # rubocop:disable AssignmentInCondition
           response << body
         end
+      end
+
+      private
+
+      def ssl_stream(client)
+        client_context = OpenSSL::SSL::SSLContext.new
+        ssl_client     = @ssl_socket_class.new(client, client_context)
+        ssl_client.connect
       end
     end
   end
